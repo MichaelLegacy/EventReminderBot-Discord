3b6b8ee16bfb35fe8985b7ee7ea14ede82b43177
Mike Legacy <mike-legacy@hotmail.com>
2019-06-25 14:12:08 -0400

    Added string parsing for event creation command

--- start.js
+++ start.js
@@ -8,6 +8,27 @@ client.on('ready', () => {
 
 client.on('message', message => {
   console.log("Message Received: " + message.content);
+
+  //Dont respond to bot messages.
+  if (message.author.bot){
+      return;
+  }
+
+  let lowerCaseMessage = message.content.toLowerCase();
+
+  // command looks like:
+  // .CreateEvent "Title here" days-hours-minutes
+  // where days-hours-minutes is the relative start time of the event from now
+  if (lowerCaseMessage.startsWith(".createevent")) {
+    let remainingMessage = message.content.slice(".CreateEvent".length, message.content.length);
+    let regexString = /("[^"]+"|[^"\s]+)/g
+    remainingMessage = remainingMessage.match(regexString);
+    remainingMessage[0] = remainingMessage[0].replace(/^"([^"]+)"$/,"$1");
+    console.log(remainingMessage);
+    message.channel.send("Event Title: " + remainingMessage[0]);
+    message.channel.send("Event time from now (days-hours-minutes): " + remainingMessage[1]);
+}
+
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
