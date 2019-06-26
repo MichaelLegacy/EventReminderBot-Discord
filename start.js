a5caa4996c02815ff2a24cc869ad3e0ca7781797
JasonShadle <jason.shadle50@gmail.com>
2019-06-26 16:00:42 -0400

    Added moment and event/role creation

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config/config.json');
 var mysql = require('mysql');
+var moment = require('moment');
 
 client.on('ready', () => {
   // connect to database
@@ -41,8 +42,40 @@ client.on('message', message => {
     remainingMessage = remainingMessage.match(regexString);
     remainingMessage[0] = remainingMessage[0].replace(/^"([^"]+)"$/,"$1");
     console.log(remainingMessage);
+    // console.log(message.id);
     message.channel.send("Event Title: " + remainingMessage[0]);
     message.channel.send("Event time from now (days-hours-minutes): " + remainingMessage[1]);
+
+    // add the user time with message time
+    regexString = /(\d)+/g
+    userTime = remainingMessage[1].match(regexString);
+    // console.log(userTime);
+    let messageTime = moment(message.createdAt);
+    // console.log(messageTime);
+    let eventTime = messageTime.add(userTime[0], 'days').add(userTime[1], 'hours').add(userTime[2], 'minutes');
+    // console.log(eventTime);
+
+    // make the role and assign to creator
+    let role = {};
+    message.guild.createRole({
+      name: 'event:' + message.id,
+      mentionable: true
+    }).catch(err => {
+      console.error(err);
+      message.channel.send('Error creating the role.');
+    }).then(function(createdRole) {
+      role = createdRole;
+      message.member.addRole(role).catch(err => {
+      console.error(err);
+      message.channel.send('Error adding member to role.');
+    })});
+    // console.log(role);
+    // message.member.addRole(role).catch(err => {
+    //   console.error(err);
+    //   message.channel.send('Error adding member to role.');
+    // });
+    //con.query('insert into ? values(?,?,?,?,?,?)', [config.database.table, message.id, remainingMessage[0], localtime,])
+
 }
 
 });
