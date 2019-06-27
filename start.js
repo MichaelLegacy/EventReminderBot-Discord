58741eeddfc972c911c5c19d5bd049244a46d876
JasonShadle <jason.shadle50@gmail.com>
2019-06-27 11:58:49 -0400

    Changed event message to bot embed, fixed reactions

--- start.js
+++ start.js
@@ -50,10 +50,9 @@ client.on('message', message => {
     let regexString = /("[^"]+"|[^"\s]+)/g
     remainingMessage = remainingMessage.match(regexString);
     remainingMessage[0] = remainingMessage[0].replace(/^"([^"]+)"$/,"$1");
-    //console.log(remainingMessage);
 
     //get neccessary data
-    let messageID = message.id;
+    let messageID;
     let eventTitle = remainingMessage[0];
     let creatorID = message.author.id;
     let serverID = message.guild.id;
@@ -62,71 +61,82 @@ client.on('message', message => {
     // add the user time with message time
     regexString = /(\d)+/g
     userTime = remainingMessage[1].match(regexString);
-    //console.log(userTime);
     let messageTime = moment(message.createdAt);
-    //console.log(messageTime);
     let eventTime = messageTime.add(userTime[0], 'days').add(userTime[1], 'hours').add(userTime[2], 'minutes');
-    //console.log(eventTime);
 
-    // make the role and assign to creator
-    message.guild.createRole({
-      name: 'event:' + messageID,
-      mentionable: true
-    }).catch(err => {
-      console.error(err);
-      message.channel.send('Error creating the role.');
-    }).then(function(createdRole) {
-      let roleID = createdRole.id;
-      message.member.addRole(roleID)
-        .catch(err => {
-          console.error(err);
-          message.channel.send('Error adding member to role.');
+    // make the bot message
+    var embed = new Discord.RichEmbed();
+    embed.setTitle(eventTitle);
+    message.channel.send(embed)
+    .catch(console.error)
+    .then((embedMessage) => {
+      embedMessage.react('✅');
+      messageID = embedMessage.id;
+      console.log(messageID);
+    }).catch(console.error)
+    .then((embed) => {
+      message.guild.createRole({
+        name: 'event:' + messageID,
+        mentionable: true
+      }).catch(err => {
+        console.error(err);
+        message.channel.send('Error creating the role.');
+      }).then(function(createdRole) {
+        let roleID = createdRole.id;
+        message.member.addRole(roleID)
+          .catch(err => {
+            console.error(err);
+            message.channel.send('Error adding member to role.');
+          })
+        SequelizeModels.event.create({
+          messageID: messageID,
+          title: eventTitle,
+          creatorID: creatorID,
+          serverID: serverID,
+          roleID: roleID,
+          active: true,
+          time: eventTime,
+          channelID: channelID
+        }).then((newEvent) => {
+            console.log('event created: ' + newEvent.eventTitle);
         })
-      SequelizeModels.event.create({
-        messageID: messageID,
-        title: eventTitle,
-        creatorID: creatorID,
-        serverID: serverID,
-        roleID: roleID,
-        active: true,
-        time: eventTime,
-        channelID: channelID
-      }).then((newEvent) => {
-          console.log('event created: ' + newEvent.eventTitle);
-      })
-        .catch(console.error);
-    });
-  }
-
-});
+          .catch(console.error);
+      });
+    })};
+  });
 
 client.on('messageReactionAdd', (reaction, user) => {
   console.log("User: " + user.username + " added reaction: " + reaction.emoji.name + "  to the message: " + reaction.message);
-  
-  SequelizeModels.event.findOne({
-    where: {
-      messageID: reaction.message.id
-    }}).then(eventMessage => {
-      // console.log(event);
-      if (eventMessage != null) {
-        reaction.message.guild.fetchMember(user).then(eventMember => {
-          eventMember.addRole(eventMessage.roleID);
-      }).catch(console.error);
-    }}).catch(console.error);
-  });
+  // only check for preset emoji
+  if (reaction.emoji.name === '✅') {
+    SequelizeModels.event.findOne({
+      where: {
+        messageID: reaction.message.id
+      }}).then(eventMessage => {
+        if (eventMessage != null) {
+          reaction.message.guild.fetchMember(user).then(eventMember => {
+            eventMember.addRole(eventMessage.roleID);
+        }).catch(console.error);
+      }}).catch(console.error);
+  }});
 
 client.on('messageReactionRemove', (reaction, user) => {
   console.log('User: ' + user.username + ' removed reaction: ' + reaction.emoji.name + ' to the message: ' + reaction.message);
-  
-  SequelizeModels.event.findOne({
-    where: {
-      messageID: reaction.message.id
-    }}).then(eventMessage => {
-      if (eventMessage != null) {
-        reaction.message.guild.fetchMember(user).then(eventMember => {
-          eventMember.removeRole(eventMessage.roleID);
+  // only check for preset emoji
+  if (reaction.emoji.name === '✅') {
+    console.log('pass name check');
+    SequelizeModels.event.findOne({
+      where: {
+        messageID: reaction.message.id
+      }}).then(eventMessage => {
+        if (eventMessage != null) {
+          console.log('deleting role');
+          reaction.message.guild.fetchMember(user).then(eventMember => {
+            eventMember.removeRole(eventMessage.roleID);
         }).catch(console.error);
       }}).catch(console.error);
-});
+  }});
+        
+
 
 client.login(token);
