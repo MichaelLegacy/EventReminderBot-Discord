5458c457046836ec817debdd34db990d3c16aed9
JasonShadle <jason.shadle50@gmail.com>
2019-06-28 20:36:56 -0400

    ✨✨ pretty ✨✨

--- start.js
+++ start.js
@@ -8,22 +8,22 @@ const moment = require('moment');
 const checkMarkUnicode = "✅";
 
 const SequelizeConnect = new Sequelize({
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      charset: 'utf8mb4'
-    },
-    pool: {
-      max: 20,
-      min: 0,
-      idle: 10000
-    },
-    define: {
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-      timestamps: true
-    }
-  });
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectOptions: {
+    charset: 'utf8mb4'
+  },
+  pool: {
+    max: 20,
+    min: 0,
+    idle: 10000
+  },
+  define: {
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+    timestamps: true
+  }
+});
 
 function setTimer(time, id) {
   console.log('Event timer start');
@@ -39,82 +39,86 @@ function endTimer(id) {
   SequelizeModels.event.findOne({
     where: {
       messageID: id
-    }}).then(eventMessage => {
-      if (eventMessage.active === true) {
-        let eventChannel = client.channels.get(eventMessage.channelID);
-        let eventServer = client.guilds.get(eventMessage.serverID);
-        let eventTitle = eventMessage.title;
-        let embed = new Discord.RichEmbed();
-        embed.setColor("#388e3c");
-        embed.setDescription("Event is starting now!");
-        embed.setAuthor(eventTitle, "https://i.imgur.com/ulaIAat.png");
-        embed.setThumbnail("https://i.imgur.com/d52hF2R.png");
+    }
+  }).then(eventMessage => {
+    if (eventMessage.active === true) {
+      let eventChannel = client.channels.get(eventMessage.channelID);
+      let eventServer = client.guilds.get(eventMessage.serverID);
+      let eventTitle = eventMessage.title;
+      let embed = new Discord.RichEmbed();
+      embed.setColor("#388e3c");
+      embed.setDescription("Event is starting now!");
+      embed.setAuthor(eventTitle, "https://i.imgur.com/ulaIAat.png");
+      embed.setThumbnail("https://i.imgur.com/d52hF2R.png");
 
-        eventChannel.send('<@&' + eventMessage.roleID + '>', embed);
-        eventChannel.fetchMessage(eventMessage.messageID)
+      eventChannel.send('<@&' + eventMessage.roleID + '>', embed);
+      eventChannel.fetchMessage(eventMessage.messageID)
         .then(message => {
-        SequelizeModels.event.update(
-          { active: false },
-          {where: {
-            messageID: id
-          }
-          
+          SequelizeModels.event.update({
+            active: false
+          }, {
+            where: {
+              messageID: id
+            }
+
+          })
+          message.edit('The event "' + eventMessage.title + '" has concluded.', embed.fields = null);
         })
-        message.edit('The event "' + eventMessage.title + '" has concluded.', embed.fields=null);
-        })
-      }
-    })
+    }
+  })
 }
 
-// TODO if time allows: fix indentation and catch errors properly
 client.on('ready', () => {
   console.log("Bot Connected");
 
   //Find all active events on restart
   SequelizeModels.event.findAll({
-    where: {'active': true}
+    where: {
+      'active': true
+    }
   }).then(function(response) {
     //for each active event fetch the associated message
     for (let i in response) {
-        let loadedEvent = response[i].dataValues;
-        // make sure the event hasn't passed yet
-        let checkTime = loadedEvent.time;
-        let currTime = moment();
-        let messageChannel = client.channels.get(loadedEvent.channelID)
-        
-        if (currTime.isAfter(checkTime)) {
-          SequelizeModels.event.update(
-            { active: false },
-            { where: {
-              messageID: loadedEvent.messageID
-            }
-          })
-          messageChannel.send('<@&' + loadedEvent.roleID + '>: Event "**' + loadedEvent.title + '**" seems to have previously started and no announcement was made.')        
-        } else {  
-          messageChannel.fetchMessage(loadedEvent.messageID)
+      let loadedEvent = response[i].dataValues;
+      // make sure the event hasn't passed yet
+      let checkTime = loadedEvent.time;
+      let currTime = moment();
+      let messageChannel = client.channels.get(loadedEvent.channelID)
+
+      if (currTime.isAfter(checkTime)) {
+        SequelizeModels.event.update({
+          active: false
+        }, {
+          where: {
+            messageID: loadedEvent.messageID
+          }
+        })
+        messageChannel.send('<@&' + loadedEvent.roleID + '>: Event "**' + loadedEvent.title + '**" seems to have previously started and no announcement was made.')
+      } else {
+        messageChannel.fetchMessage(loadedEvent.messageID)
           .then((eventMessage) => {
             // Filter to only the ✅ reaction (there's only 1)
             let checkmarkReact = eventMessage.reactions
               .filter(reaction => {
                 return reaction._emoji.name == checkMarkUnicode;
-            }).first();
+              }).first();
             //fetch the users that used the reaction
             checkmarkReact.fetchUsers()
               .then((reactionUsers) => {
                 for (let reactUserID of reactionUsers.keys()) {
-                    //get the user and the associated guildMember
-                    client.fetchUser(reactUserID)
-                      .then((user) =>{
-                          if (!user.bot) {
-                            eventMessage.guild.fetchMember(user)
-                              // add to the role if they don't have it
-                              .then((guildMember) => {
-                                  if (guildMember.roles.find(role => role.id === loadedEvent.roleID) == null){
-                                      guildMember.addRole(loadedEvent.roleID);
-                                  }
-                              });
-                          }
-                      });
+                  //get the user and the associated guildMember
+                  client.fetchUser(reactUserID)
+                    .then((user) => {
+                      if (!user.bot) {
+                        eventMessage.guild.fetchMember(user)
+                          // add to the role if they don't have it
+                          .then((guildMember) => {
+                            if (guildMember.roles.find(role => role.id === loadedEvent.roleID) == null) {
+                              guildMember.addRole(loadedEvent.roleID);
+                            }
+                          });
+                      }
+                    });
                 };
                 // get role and check all users with the role
                 let eventRole = eventMessage.guild.roles.find(role => role.id == loadedEvent.roleID);
@@ -122,25 +126,26 @@ client.on('ready', () => {
                   let guildUser = guildMember[1].user;
 
                   //get array of IDs only
-                  let reactionUserIDsArr = reactionUsers.map((reactionUser)=> {
-                      return reactionUser.id;
+                  let reactionUserIDsArr = reactionUsers.map((reactionUser) => {
+                    return reactionUser.id;
                   });
 
                   //remove role if user is not currently reacted to the event
                   if (!reactionUserIDsArr.includes(guildUser.id)) {
-                      guildMember[1].removeRole(eventRole.id)
-                        .then((removedMember) => {
-                            console.log("A role Was Removed from: " + guildUser.username + " they removed their reaction.")
-                        })
-                        .catch(console.error);
+                    guildMember[1].removeRole(eventRole.id)
+                      .then((removedMember) => {
+                        console.log("A role Was Removed from: " + guildUser.username + " they removed their reaction.")
+                      })
+                      .catch(console.error);
                   }
                 };
               });
-            });
+          });
         // set timers for active events
         setTimer(loadedEvent.time, loadedEvent.messageID);
-    };
-  }});
+      };
+    }
+  });
 });
 
 client.on('message', message => {
@@ -148,8 +153,8 @@ client.on('message', message => {
   console.log("Message Received: " + message.content);
 
   //Dont respond to bot messages.
-  if (message.author.bot){
-      return;
+  if (message.author.bot) {
+    return;
   }
 
   if (!message.member.roles.find(role => role.name.toLowerCase() === "eventbotplanner") && !message.member.hasPermission("ADMINISTRATOR")) {
@@ -165,7 +170,7 @@ client.on('message', message => {
     let remainingMessage = message.content.slice(".CreateEvent".length, message.content.length);
     let regexString = /("[^"]+"|[^"\s]+)/g
     remainingMessage = remainingMessage.match(regexString);
-    remainingMessage[0] = remainingMessage[0].replace(/^"([^"]+)"$/,"$1");
+    remainingMessage[0] = remainingMessage[0].replace(/^"([^"]+)"$/, "$1");
 
     //get neccessary data
     let messageID;
@@ -190,44 +195,46 @@ client.on('message', message => {
     embed.setFooter(footerMessage);
 
     message.channel.send("Event Scheduled", embed)
-    .catch(console.error)
-    .then((embedMessage) => {
-      embedMessage.react('✅');
-      messageID = embedMessage.id;
-      console.log(messageID);
-      message.channel.send("The event creator can delete the event using `.deleteevent " + messageID + "`.");
-    }).catch(console.error)
-    .then((embed) => {
-      message.guild.createRole({
-        name: 'event:' + messageID,
-        mentionable: true,
-        permissions: 0
-      }).catch(err => {
-        console.error(err);
-        message.channel.send('Error creating the role.');
-      }).then(function(createdRole) {
-        let roleID = createdRole.id;
-        SequelizeModels.event.create({
-          messageID: messageID,
-          title: eventTitle,
-          creatorID: creatorID,
-          serverID: serverID,
-          roleID: roleID,
-          active: true,
-          time: eventTime,
-          channelID: channelID
-        }).then((newEvent) => {
-            // console.log('event created: ' + newEvent.eventTitle);
-            setTimer(eventTime, messageID);
-        })
-          .catch(console.error);
+      .catch(console.error)
+      .then((embedMessage) => {
+        embedMessage.react('✅');
+        messageID = embedMessage.id;
+        console.log(messageID);
+        message.channel.send("The event creator can delete the event using `.deleteevent " + messageID + "`.");
+      }).catch(console.error)
+      .then((embed) => {
+        message.guild.createRole({
+          name: 'event:' + messageID,
+          mentionable: true,
+          permissions: 0
+        }).catch(err => {
+          console.error(err);
+          message.channel.send('Error creating the role.');
+        }).then(function(createdRole) {
+          let roleID = createdRole.id;
+          SequelizeModels.event.create({
+              messageID: messageID,
+              title: eventTitle,
+              creatorID: creatorID,
+              serverID: serverID,
+              roleID: roleID,
+              active: true,
+              time: eventTime,
+              channelID: channelID
+            }).then((newEvent) => {
+              // console.log('event created: ' + newEvent.eventTitle);
+              setTimer(eventTime, messageID);
+            })
+            .catch(console.error);
+        });
       });
-    });
-    } else if ((lowerCaseMessage === ".removeinactiveroles") && message.member.hasPermission("ADMINISTRATOR")) {
+  } else if ((lowerCaseMessage === ".removeinactiveroles") && message.member.hasPermission("ADMINISTRATOR")) {
     //Find all active events on restart
     let server = message.guild;
     SequelizeModels.event.findAll({
-        where: {'active': false}
+      where: {
+        'active': false
+      }
     }).then(function(response) {
       let deletedRoleCount = 0;
       //for each active event fetch the associated message
@@ -242,9 +249,9 @@ client.on('message', message => {
       }
       message.reply("Deleted " + deletedRoleCount + " inactive event roles.");
     })
-    } else if (lowerCaseMessage.startsWith(".deleteevent")) {
-      let id = lowerCaseMessage.split(' ')[1];
-      SequelizeModels.event.findOne({
+  } else if (lowerCaseMessage.startsWith(".deleteevent")) {
+    let id = lowerCaseMessage.split(' ')[1];
+    SequelizeModels.event.findOne({
         where: {
           messageID: id
         }
@@ -253,18 +260,20 @@ client.on('message', message => {
         // console.log(eventMessage);
         let messageChannel = client.channels.get(eventMessage.channelID)
         if ((eventMessage.creatorID === message.author.id) || message.member.hasPermission("ADMINISTRATOR")) {
-          if (eventMessage.active === true ) {
+          if (eventMessage.active === true) {
             messageChannel.fetchMessage(eventMessage.messageID)
-            .then((botMessage) => {
-              SequelizeModels.event.update (
-                { active: false },
-                { where: {
+              .then((botMessage) => {
+                SequelizeModels.event.update({
+                  active: false
+                }, {
+                  where: {
                     messageID: id
-                }})
+                  }
+                })
 
-              messageChannel.send('Event "' + eventMessage.title + '" has been deleted.');
-              botMessage.delete()
-            })
+                messageChannel.send('Event "' + eventMessage.title + '" has been deleted.');
+                botMessage.delete()
+              })
           } else {
             messageChannel.send('Event "' + eventMessage.title + '" has already been removed or it has already occured.')
           }
@@ -272,33 +281,33 @@ client.on('message', message => {
           messageChannel.send("You don't have permission to delete that event.")
         }
       })
-    } else if (lowerCaseMessage === ".help") {
-        message.channel.send({
-         "embed": {
-            "color": 16777215,
-            "author": {
-              "name": "Command List",
-              "icon_url": "https://i.imgur.com/ulaIAat.png"
-            },
-            "description": "To create an event, you must have a role named `eventbotplanner` or have the `Administrator` permission.",
-            "fields": [
-            {
-              "name": ".CreateEvent",
-              "value": "Use `.createevent \"[event title]\" [days]-[hours]-[minutes]` to create an event."
-            },
-            {
-              "name": ".DeleteEvent",
-              "value": "Event creator or admin can use `.deleteevent [eventMessageID]` to delete an event."
-            },
-            {
-              "name": ".RemoveInactiveRoles",
-              "value": "Server administrators may use this to remove all roles created for events that have already happened."
-            }]
-         }
-      });
-    }
+  } else if (lowerCaseMessage === ".help") {
+    message.channel.send({
+      "embed": {
+        "color": 16777215,
+        "author": {
+          "name": "Command List",
+          "icon_url": "https://i.imgur.com/ulaIAat.png"
+        },
+        "description": "To create an event, you must have a role named `eventbotplanner` or have the `Administrator` permission.",
+        "fields": [{
+            "name": ".CreateEvent",
+            "value": "Use `.createevent \"[event title]\" [days]-[hours]-[minutes]` to create an event."
+          },
+          {
+            "name": ".DeleteEvent",
+            "value": "Event creator or admin can use `.deleteevent [eventMessageID]` to delete an event."
+          },
+          {
+            "name": ".RemoveInactiveRoles",
+            "value": "Server administrators may use this to remove all roles created for events that have already happened."
+          }
+        ]
+      }
+    });
+  }
 });
-  
+
 client.on('messageReactionAdd', (reaction, user) => {
   console.log("User: " + user.username + " added reaction: " + reaction.emoji.name + "  to the message: " + reaction.message);
   // only check for preset emoji
@@ -306,13 +315,16 @@ client.on('messageReactionAdd', (reaction, user) => {
     SequelizeModels.event.findOne({
       where: {
         messageID: reaction.message.id
-      }}).then(eventMessage => {
-        if (eventMessage != null) {
-          reaction.message.guild.fetchMember(user).then(eventMember => {
-            eventMember.addRole(eventMessage.roleID);
+      }
+    }).then(eventMessage => {
+      if (eventMessage != null) {
+        reaction.message.guild.fetchMember(user).then(eventMember => {
+          eventMember.addRole(eventMessage.roleID);
         }).catch(console.error);
-      }}).catch(console.error);
-  }});
+      }
+    }).catch(console.error);
+  }
+});
 
 client.on('messageReactionRemove', (reaction, user) => {
   console.log('User: ' + user.username + ' removed reaction: ' + reaction.emoji.name + ' to the message: ' + reaction.message);
@@ -322,15 +334,18 @@ client.on('messageReactionRemove', (reaction, user) => {
     SequelizeModels.event.findOne({
       where: {
         messageID: reaction.message.id
-      }}).then(eventMessage => {
-        if (eventMessage != null) {
-          console.log('deleting role');
-          reaction.message.guild.fetchMember(user).then(eventMember => {
-            eventMember.removeRole(eventMessage.roleID);
+      }
+    }).then(eventMessage => {
+      if (eventMessage != null) {
+        console.log('deleting role');
+        reaction.message.guild.fetchMember(user).then(eventMember => {
+          eventMember.removeRole(eventMessage.roleID);
         }).catch(console.error);
-      }}).catch(console.error);
-  }});
-   
+      }
+    }).catch(console.error);
+  }
+});
+
 
 
 client.login(token);
